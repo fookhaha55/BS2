ecab04cc8405711e88fe1a6633a9f14d59850434
unknown <puvadonsang@gmail.com>
2021-11-23 16:54:15 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BS2';
+  n1: number = 5;
+  n2: number = 2;
+  picName:string = "https://s.isanook.com/au/0/ud/15/77967/lamborghinihuracansto6.jpg";
+  hi(name:string){
+    alert("Hi " + name);
+  }
 }
